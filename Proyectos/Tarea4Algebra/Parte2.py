59205785bbede3e52538e130bf5fda47568de7ea
brunoalbin23 <albinbruno23@gmail.com>
2024-11-17 13:25:51 -0300

    Update parte 2

--- Proyectos/Tarea4Algebra/Parte2.py
+++ Proyectos/Tarea4Algebra/Parte2.py
@@ -1,6 +1,6 @@
-def pasar_a_numeros_de3(cadena):  #Esta funcion devuelve una lista con los caracteres agrupados de a 3
+def agrupar_de3(cadena):  #Esta funcion devuelve una lista con los caracteres agrupados de a 3
     lista = []
-    sub_lista =[]
+    sub_lista = []
     contador = 0
     for i in cadena:
         if contador == 3:
@@ -17,4 +17,26 @@ def pasar_a_numeros_de3(cadena):  #Esta funcion devuelve una lista con los carac
     return lista
 
 
-print(pasar_a_numeros_de3("colombia lo empato en el minuto noventa y seis y uruguay lo gano sacando del medio en el minuto ciento uno"))
+def pasar_a_numeros(lista):
+    for j in lista:
+        for i in range(0, 3):
+            if j[i] == " ":
+                j[i] = 27
+            elif j[i] == "*":
+                j[i] = 28
+            elif j[i] == "ñ":
+                j[i] = 14
+            else:
+                valor = (ord(j[i]) - ord('a'))
+                if valor < 14:
+                    j[i] = valor
+                else:
+                    j[i] = valor + 1
+    return lista
+
+
+texto_de3 = agrupar_de3(
+    "colombia lo empato en el minuto noventa y seis y uruguay lo gano sacando del medio en el minuto ciento uno")
+texto_de3_numerico = pasar_a_numeros(texto_de3)
+
+print(texto_de3_numerico)
